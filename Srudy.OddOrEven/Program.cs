384654952b82b25f05b09f35e364471d1f42426a
Eduardo Henrique <newdamage1@live.com>
2024-02-10 15:46:12 -0300

    Adicionar arquivos de projeto.

---
+++ Srudy.OddOrEven/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Srudy.OddOrEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int r;
+
+            Console.WriteLine("This program verify the number and tell us if the number is odd ou even.");
+            Console.WriteLine("Tell us the number");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            r = n % 2;
+
+            if (r > 0)
+            {
+                Console.WriteLine("The number {0} is odd.", n);
+            } if (r == 0)
+            {
+                Console.WriteLine("The number {0} is even", n);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
